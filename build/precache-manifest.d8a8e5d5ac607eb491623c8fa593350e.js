70f4ab03b9a5402963fd5a544e5add3f4c271689
Zacarias Jorge Torrezão <z.torrezao@gmail.com>
2019-12-04 16:59:25 +0200

    built files

---
+++ build/precache-manifest.d8a8e5d5ac607eb491623c8fa593350e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9363b9ef0f176fdf0244b810b770b011",
+    "url": "/index.html"
+  },
+  {
+    "revision": "907036fcd8c78667c94c",
+    "url": "/static/js/2.a08b6de6.chunk.js"
+  },
+  {
+    "revision": "8bf7a932198ede56b202",
+    "url": "/static/js/main.abf684fd.chunk.js"
+  },
+  {
+    "revision": "da02b68f55d6f2641a4d",
+    "url": "/static/js/runtime-main.b0a7a006.js"
+  }
+]);
